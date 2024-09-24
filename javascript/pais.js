2b2989afd572482440163284df3fac827e3997e3
Sonilia Policarpo <sonilia.nascimento@etec.sp.gov.br>
2024-09-23 22:18:43 -0300

    Aula 23/09

---
+++ javascript/pais.js
@@ -0,0 +1,49 @@
+Welcome to Node.js v22.3.0.
+Type ".help" for more information.
+> var num1 = 5
+undefined
+> var num2 = 7
+undefined
+> var num3 = 17
+undefined
+> var x = num1 + num2
+undefined
+> console.log(x)
+12
+undefined
+> var s = num1 % num2
+undefined
+> console.log(s)
+5
+undefined
+> var e = num3**3
+undefined
+> console.log(e)
+4913
+undefined
+> console.log('Pré-incremento' , ++num1)
+Pré-incremento 6
+undefined
+> console.log('Pós-incremento' , num1++)
+Pós-incremento 6
+undefined
+> console.log(num1)
+7
+undefined
+> console.log('Pós-6' , num1+9)
+Pós-6 16
+undefined
+> console.log(num1)
+7
+undefined
+> var num1= 19
+undefined
+> console.log('Pre-decremento' , --num1)
+Pre-decremento 18
+undefined
+> console.log('Pos-incremento' , num1--)
+Pos-incremento 18
+undefined
+> console.log(num1)
+17
+undefined
