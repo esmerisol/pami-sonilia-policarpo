45ff2a3a96d305abc35d5911e2c3032f05c2abbc
Sonilia Plicarpo <sonilia.nascimento@etec.sp.gov.br>
2024-09-30 22:26:16 -0300

    while , for e function

---
+++ aula3/function.js
@@ -0,0 +1,13 @@
+
+
+
+
+function mult (m){
+
+    return a * b
+}
+
+a = 7
+b = 9
+
+console.log(mult (a,b))
---
+++ aula3/funçãotab.js
@@ -0,0 +1,9 @@
+console.log(tabuada(8))
+function tabuada (t){
+
+    for(let i = 0 ; i <= 10 ; i++){
+
+        return( t * i)
+
+    }
+}
